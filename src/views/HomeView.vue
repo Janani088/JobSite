4dfe745227ed4eb8aa2278c8af3c78476c1b5921
Sri Janani Ramesh <k97a5@unb.ca>
2025-01-26 23:25:06 -0500

    commit commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from './components/Joblistings.vue';
+</script>
+
+<template>
+ <Hero title="Test Title" subtitle="Test Subtitle"/>
+  <HomeCards />
+  <JobListings :limit="3" :showButton="true"/>
+</template>
